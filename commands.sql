cc78336cc2ef03d697238542154f4c1e824a0895
Boerescu Constantin <costi.boerescu14@gmail.com>
2023-07-31 20:42:17 +0300

    Final

--- commands.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL);
-INSERT INTO users (username, hash) VALUES ('user1', 'pass1');
-SELECT * FROM users;
-
-CREATE TABLE cats (id_cat INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT NOT NULL, age TEXT NOT NULL, gender TEXT NOT NULL, location TEXT NOT NULL, description TEXT NOT NULL, photo_path TEXT NOT NULL, user_id INTEGER, FOREIGN KEY (user_id) REFERENCES users(id));
-
-name, age, gender, location, description, photo_path
